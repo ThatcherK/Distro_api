799ca54e000de46526122b4351b77990a36a6e1a
ThatcherK <kaliithatmau@gmail.com>
2020-12-29 12:07:14 +0300

    Initial commit

---
+++ app/__init__.py
@@ -0,0 +1,28 @@
+import os
+from flask import Flask,current_app
+from flask_bcrypt import Bcrypt
+from flask_cors import CORS
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+import config
+
+db = SQLAlchemy()
+migrate = Migrate()
+bcrypt = Bcrypt()
+
+def create_app(script_info=None):
+    app = Flask(__name__)
+    app.config.from_object(config.DevelopmentConfig)
+
+    db.init_app(app)
+    CORS(app)
+    bcrypt.init_app(app)
+    migrate.init_app(app, db)
+
+    # shell context for flask cli
+    @app.shell_context_processor
+    def ctx():
+        return {"app": app, "db": db}
+
+    return app
