77be99903002d5089a21dd300c8bfc4ced014a55
ThatcherK <kaliithatmau@gmail.com>
2020-12-29 23:23:11 +0300

    Seed the database

--- app/api/views.py
+++ app/api/views.py
@@ -1,18 +1,31 @@
+from uuid import uuid4
+
 from flask import Blueprint, current_app, request
 from flask_restx import Api, Resource
 
 from app import bcrypt, db
-from app.api.models import User
+from app.api.models import User, InvitedUser
 
 users_blueprint = Blueprint("users", __name__)
 api = Api(users_blueprint)
 
-class RegisterUser(Resource):
+class InviteUser(Resource):
     def post(self):
         post_data = request.get_json()
-        username = post_data.get("username")
-        password = post_data.get("password")
+        email = post_data.get("email")
         role_id = post_data.get("role_id")
-        business_id = post_data.get("business_id")
-        
+        if (email  and role_id) == "":
+            response_object = {"message": "Invalid payload"}
+            return response_object, 400
+        invite_code = str(uuid4())
+        invited_user = InvitedUser(email, invite_code, role_id)
+        invited_user.save()
+        if current_app.config != "testing":
+            send_invite_email(invite_code, invited_user.email, "http://localhost:3000/")
+        return {"invitedUser": invitedUser.json()}, 200
 
+    def get(self):
+        invited_users = InvitedUser.query.all()
+        return {"invited_users": [user.json() for user in invited_users]}, 200   
+
+api.add_resource(InviteUser, "/invited_users")
--- manage.py
+++ manage.py
@@ -2,10 +2,22 @@ from flask.cli import FlaskGroup
 from sqlalchemy import text
 
 from app import create_app, db
+from app.api.models import Role, Status
 
 app = create_app()
 cli = FlaskGroup(create_app=create_app)
 
+def create_status():
+    db.session.add(Status(name="ordered"))
+    db.session.add(Status(name="transit"))
+    db.session.add(Status(name="delivered"))
+
+def create_roles():
+    db.session.add(Role(name="business_owner"))
+    db.session.add(Role(name="transport_manager"))
+    db.session.add(Role(name="driver"))
+    db.session.add(Role(name="store_manager"))
+    db.session.add(Role(name="customer"))
 
 def drop_all_tables():
     """ drops all tables and sequences from a postgres database"""
@@ -23,5 +35,10 @@ def recreate_db():
     db.create_all()
     db.session.commit()
 
+@cli.command("seed_db")
+def seed_db():
+    create_status()
+    create_roles()
+    db.session.commit()
 if __name__ == "__main__":
     cli()
