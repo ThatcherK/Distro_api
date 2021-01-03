9240c5cb970f11e76484aa9b5ece6d75d22307b0
ThatcherK <kaliithatmau@gmail.com>
2021-01-03 08:00:30 +0300

    Add business tests

--- app/api/views/business.py
+++ app/api/views/business.py
@@ -2,34 +2,35 @@ from flask import Blueprint, current_app, request
 from flask_restx import Api, Resource
 
 from app.api.models import Business, User
+from app.api.utils import requires_admin_access
+
 
 business_blueprint = Blueprint("businesses", __name__)
 api = Api(business_blueprint)
 
 class BusinessView(Resource):
+    @requires_admin_access
     def post(self):
         post_data = request.get_json()
         header = request.headers.get("Authorization")
         name = post_data.get("name")
         auth_token = header.split(' ')[1]
-        print(auth_token)
         user_id = User.decode_auth_token(auth_token)
-        print(user_id)
         user = User.query.filter_by(id=user_id).first()
         response_object = {}
         if user:
-            if user.role_id == 1:
-                business = Business(name)
-                business.save()
-                response_object = {
-                    "message": "success",
-                    "business": business.json()
-                }
-                return response_object, 201
+            # if user.role_id == 1:
+            business = Business(name)
+            business.save()
+            response_object = {
+                "message": "success",
+                "business": business.json()
+            }
+            return response_object, 201
         response_object = {
             "message": "Unauthorized"
         }
-        return response_object, 404
+        return response_object, 401
 
 
     def get(self):
---
+++ tests/test_business.py
@@ -0,0 +1,33 @@
+import json
+
+def test_add_business(test_app, test_database):
+    test_token = test_database[1]
+    client = test_app.test_client()
+    resp = client.post(
+        "/business",
+        data=json.dumps({"name": "distro"}),
+        content_type="application/json",
+        headers={"Authorization": f"Bearer {test_token}"},
+    )
+    data = json.loads(resp.data.decode())
+
+    assert resp.status_code == 201
+    assert "distro" in data["business"].get("name")
+
+def test_add_business_no_authtoken(test_app):
+    client = test_app.test_client()
+    resp = client.post(
+        "/business",
+        data=json.dumps({"name": "distro"}),
+        content_type="application/json",
+    )
+    data = json.loads(resp.data.decode())
+
+    assert resp.status_code == 401
+
+def test_get_business(test_app):
+    # client = test_app.test_client()
+    # resp = client.get("/business")
+    # data = json.loads(resp.data.decode())
+
+    # assert resp.status_code == 200
