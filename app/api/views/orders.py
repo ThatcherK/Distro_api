d9bc5137a17571b1d845fd767f6ef16bc55043b0
ThatcherK <kaliithatmau@gmail.com>
2021-01-04 12:00:21 +0300

    Add inventory update after order

--- app/api/views/orders.py
+++ app/api/views/orders.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, current_app, request
 from flask_restx import Api, Resource
 
-from app.api.models import Order
+from app.api.models import Order, Inventory
 orders_blueprint = Blueprint("orders", __name__)
 api = Api(orders_blueprint)
 
@@ -11,14 +11,27 @@ class OrderView(Resource):
         inventory_id = post_data.get("inventory_id")
         quantity = post_data.get("quantity")
         customer_id = post_data.get("customer_id")
-        response_object = {}
-        order = Order(inventory_id, quantity, customer_id, 1)
-        order.save()
+        inventory_item = Inventory.query.filter_by(id=inventory_id)
+        if inventory_item:
+            if inventory_item.quantity > quantity:
+                order = Order(inventory_id, quantity, customer_id, 1)
+                order.save()
+                inventory_item.quantity -= quantity
+                inventory_item.save()
+                response_object = {
+                    "message": "success",
+                    "order": order.json()
+                }
+                return response_object, 201
+            else:
+                response_object = {
+                    "message": "Required quantity exceeds the stock"
+                }
+                response_object, 400
         response_object = {
-            "message": "success",
-            "order": order.json()
+            "message": "Not found"
         }
-        return response_object, 201
+        response_object, 404
     def get(self):
         orders = Order.query.all()
         return {"orders": [order.json() for order in orders]}, 200
